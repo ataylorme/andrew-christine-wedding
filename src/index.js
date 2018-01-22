59ab09b65722adacd8eade19d28802e666e83ef5
ataylorme <andrew@ataylor.me>
2018-01-22 08:00:30 -0800

    Add Modernizer to detect WebP support
    Detect WebP support with a custom Modernizr build. Default to standard JPEG images for announcement background. Use WebP when supported.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 require('typeface-playfair-display');
+require('./utils/modernizr-custom.js');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
