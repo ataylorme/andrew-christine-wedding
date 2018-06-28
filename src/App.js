9be6112611b6df8fc599926a936914defe9185f5
ataylorme <andrew@ataylor.me>
2018-06-28 16:08:55 -0700

    Add date to header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header.js'
+import EngagementPhoto from './components/EngagementPhoto/EngagementPhoto.js'
+import { Container } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
+      <Container id="main">
         <Header />
-      </div>
+        <EngagementPhoto />
+      </Container>
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './Header.css';
 import { Col, Row } from 'reactstrap';
 import ReactFitText from 'react-fittext';
@@ -9,11 +9,16 @@ export default class Header extends Component {
         <Row id="header">
             <Col>
                 <ReactFitText>
-                    <h1 className="display-3 text-center">
-                        Andrew Taylor
-                        <span className="and">and</span>
-                        Christine Russell
-                    </h1>
+                        <h1 className="display-3 text-center">
+                            Andrew Taylor
+                            <span className="and">and</span>
+                            Christine Russell
+                        </h1>
+                </ReactFitText>
+                <ReactFitText maxFontSize="75px">
+                    <h2 className="display-3 text-center">
+                        May 4th, 2019
+                    </h2>
                 </ReactFitText>
             </Col>
         </Row>
