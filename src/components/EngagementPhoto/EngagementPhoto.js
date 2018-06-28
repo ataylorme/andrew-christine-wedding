031be478a42a782c7631df58dede6065c90e9772
ataylorme <andrew@ataylor.me>
2018-06-28 16:03:33 -0700

    Move engagement photo to it's own component

---
+++ src/components/EngagementPhoto/EngagementPhoto.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './EngagementPhoto.css';
+import { Col, Row } from 'reactstrap';
+import EngagementPhoto2200 from './andrew-christine-engagement-12-09-2017-2200.jpg';
+import EngagementPhoto1100 from './andrew-christine-engagement-12-09-2017-1100.jpg';
+import EngagementPhoto640 from './andrew-christine-engagement-12-09-2017-640.jpg';
+import EngagementPhoto400 from './andrew-christine-engagement-12-09-2017-400.jpg';
+
+export default class Header extends Component {
+  render() {
+    return (
+        <Row id="engagement-photo" className="text-center">
+            <Col>
+            {/*
+            ​<picture>
+                <source
+                    srcSet={`${EngagementPhoto400} 400w, ${EngagementPhoto640} 640w, ${EngagementPhoto1100} 1100w, ${EngagementPhoto2200} 2200w" alt="Andrew Taylor and Christine Russell Engagment`} 
+                    />
+                <img src={EngagementPhoto400} className="img-fluid" />
+            ​</picture>
+            */}
+            <img src={EngagementPhoto400} 
+                width="100%"
+                srcSet={`${EngagementPhoto400} 400w, ${EngagementPhoto640} 640w, ${EngagementPhoto1100} 1100w, ${EngagementPhoto2200} 2200w" alt="Andrew Taylor and Christine Russell Engagment`} />
+            </Col>
+        </Row>
+    );
+  }
+}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,22 @@
 import React, { Component } from 'react';
 import './Header.css';
-import { Container, Col, Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import ReactFitText from 'react-fittext';
-import EngagementPhoto2200 from './andrew-christine-engagement-12-09-2017-2200.jpg';
-import EngagementPhoto1100 from './andrew-christine-engagement-12-09-2017-1100.jpg';
-import EngagementPhoto640 from './andrew-christine-engagement-12-09-2017-640.jpg';
-import EngagementPhoto400 from './andrew-christine-engagement-12-09-2017-400.jpg';
 
 export default class Header extends Component {
   render() {
     return (
-        <Container>
-            <header id="header">
-                <Row>
-                    <Col>
-                        <ReactFitText>
-                            <h1 className="display-3 text-center">
-                                Andrew Taylor
-                                <span className="and">and</span>
-                                Christine Russell
-                            </h1>
-                        </ReactFitText>
-                    </Col>
-                </Row>
-            </header>
-            <div id="engagement-photo" className="text-center">
-                <Row>
-                    <Col>
-                    ​<picture>
-                        <source
-                            srcSet={`${EngagementPhoto400} 400w, ${EngagementPhoto640} 640w, ${EngagementPhoto1100} 1100w, ${EngagementPhoto2200} 2200w" alt="Andrew Taylor and Christine Russell Engagment`} 
-                            />
-                        <img src={EngagementPhoto400} className="img-fluid" />
-                    ​</picture>
-                    {/*
-                    <img src={EngagementPhoto400} 
-                        width="100%"
-                        srcSet={`${EngagementPhoto400} 400w, ${EngagementPhoto640} 640w, ${EngagementPhoto1280} 1280w, ${EngagementPhoto2560} 2560w" alt="Andrew Taylor and Christine Russell Engagment`} 
-                        className="rounded"
-                        />
-                    */}
-                    </Col>
-                </Row>
-            </div>
-        </Container>
+        <Row id="header">
+            <Col>
+                <ReactFitText>
+                    <h1 className="display-3 text-center">
+                        Andrew Taylor
+                        <span className="and">and</span>
+                        Christine Russell
+                    </h1>
+                </ReactFitText>
+            </Col>
+        </Row>
     );
   }
 }
