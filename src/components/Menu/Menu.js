efa0c52c274a928ef4be5f99b71fd638f590afac
ataylorme <andrew@ataylor.me>
2018-10-26 11:22:16 -0600

    Remove fit text

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,34 +1,27 @@
 import React, { Component } from 'react';
 import './Menu.css';
 import { Container, Nav, NavItem, NavLink } from 'reactstrap';
-import ReactFitText from 'react-fittext';
 
 export default class Menu extends Component {
   render() {
     return (
         <Container id="menu" className="text-center">
             <Nav horizontal="true">
-                <ReactFitText minFontSize={24} maxFontSize={48}>
-                    <NavItem  className="col-4">
-                        <NavLink href="#venue">
-                            Venue
-                        </NavLink>
-                    </NavItem>
-                </ReactFitText>
-                <ReactFitText minFontSize={24} maxFontSize={48}>
-                    <NavItem  className="col-4">
-                        <NavLink href="#timeline">
-                            Our Story
-                        </NavLink>
-                    </NavItem>
-                </ReactFitText>
-                <ReactFitText minFontSize={24} maxFontSize={48}>
-                    <NavItem  className="col-4">
-                        <NavLink href="#rsvp">
-                            RSVP
-                        </NavLink>
-                    </NavItem>
-                </ReactFitText>
+                <NavItem  className="col-4">
+                    <NavLink href="#venue">
+                        Venue
+                    </NavLink>
+                </NavItem>
+                <NavItem  className="col-4">
+                    <NavLink href="#timeline">
+                        Our Story
+                    </NavLink>
+                </NavItem>
+                <NavItem  className="col-4">
+                    <NavLink href="#rsvp">
+                        RSVP
+                    </NavLink>
+                </NavItem>
             </Nav>
         </Container>
     );
