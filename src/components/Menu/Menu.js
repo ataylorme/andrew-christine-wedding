5a3186e1b79e275109d743f149cbd2860b70f30d
ataylorme <andrew@ataylor.me>
2018-12-17 17:03:05 -0800

    Add registry link

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,19 +7,24 @@ export default class Menu extends Component {
     return (
         <Container id="menu" className="text-center">
             <Nav>
-                <NavItem  className="col-4">
-                    <NavLink href="#venue">
+                <NavItem className="col-6 col-md-3">
+                    <NavLink href="#venue" className="no-border">
                         Venue
                     </NavLink>
                 </NavItem>
-                <NavItem  className="col-4">
-                    <NavLink href="#timeline">
+                <NavItem className="col-6 col-md-3">
+                    <NavLink href="#timeline" className="no-border">
                         Our Story
                     </NavLink>
                 </NavItem>
-                <NavItem  className="col-4">
+                <NavItem className="col-6 col-md-3">
                     <NavLink href="#rsvp">
                         RSVP
+                    </NavLink>
+                </NavItem>
+                <NavItem className="col-6 col-md-3">
+                    <NavLink href="https://www.zola.com/registry/taylorrussellwedding/">
+                        Registry
                     </NavLink>
                 </NavItem>
             </Nav>
